07efef979781179a0e18a056df06dd6da5000042
PanagiotisChytas <chytas96@gmail.com>
2017-03-03 08:38:54 -0800

    ksexwrista thread kai app gia rcv

--- hw1/netfifo.py
+++ hw1/netfifo.py
@@ -2,6 +2,8 @@
     API for FIFO pipe with UDP/IP connection
 """
 
+PACKET_SIZE = 20
+
 import sys
 #the socket dir contains MySocket_library.py
 sys.path.append('../sockets/')
@@ -13,73 +15,101 @@ import struct
 import thread
 import threading
 
+fd_list = []
+rcv_buffer_size = 0
+#buffer (dictionary [key: payload])
+rcv_buf = {}
+
+################################# RCV ################################# 
 #mutex for app. If the packet it requests isnt in the buffer app_wait = 1 and app_wait_mtx.acquire()
-app_wait_mtx = threading.Lock()
-app_wait_mtx.acquire()
-app_wait = 0
+rcv_app_wait_mtx = threading.Lock()
+rcv_app_wait_mtx.acquire()
+rcv_app_wait = 0
 
 #mutex for sychronization between thread and app
-thread_app_mtx = threading.Lock()
+rcv_thread_app_mtx = threading.Lock()
 
 #next packet app wants to read from buffer (starts from 1)
-next_app_read = 1
+rcv_next_app_read = 1
 #next packet thread wants to read from socket (starts from 1)
-next_waiting = 1
+rcv_next_waiting = 1
 #number of packets in buffer
-in_buffer = 0
-#buffer (dictionary [key: payload])
-buf = {}
+rcv_in_buffer = 0
+################################# RCV #################################
+
+
+
+################################# SEND ################################# 
+#mutex for app. If the packet it requests isnt in the buffer app_wait = 1 and app_wait_mtx.acquire()
+snd_app_wait_mtx = threading.Lock()
+snd_app_wait_mtx.acquire()
+snd_app_wait = 0
+
+#mutex for sychronization between thread and app
+snd_thread_app_mtx = threading.Lock()
+
+#next packet app wants to read from buffer (starts from 1)
+snd_next_app_read = 1
+#next packet thread wants to read from socket (starts from 1)
+snd_next_waiting = 1
+#number of packets in buffer
+snd_in_buffer = 0
+################################# SEND #################################
+
+
+
 
 
 #thread code
 def rcv_thread (sock):
 	
-	global next_waiting
-	global next_app_read
-	global in_buffer
-	global buffer_size
+	global rcv_next_waiting
+	global rcv_next_app_read
+	global rcv_in_buffer
+	global rcv_buffer_size
 	
 	while (True):
 		
 		unlock = True
 		
-		
 		print "Thread waits"
 		
 		
 		#wait for next packet
-		data = deconstruct_packet(sock.ReceiveFrom(20)[0])
+		data = deconstruct_packet(sock.ReceiveFrom(PACKET_SIZE)[0])
 		
 		
 		print "Thread:", data
 		
-		thread_app_mtx.acquire()
+		rcv_thread_app_mtx.acquire()
 		
+		
+		print data[1], rcv_next_waiting, rcv_in_buffer, rcv_buffer_size
 		
 		#if it isnt the next packet or buffer is full throw it 
 		#else update buffer
-		if (data[1] == next_waiting and in_buffer < buffer_size):
-			buf.update( {next_waiting: data[2]} )
-			next_waiting += 1
-			in_buffer += 1
-			print "Thread: Received packet" ,data[1] , in_buffer, "/", buffer_size
+		if (data[1] == rcv_next_waiting and rcv_in_buffer < rcv_buffer_size):
+			rcv_buf.update( {rcv_next_waiting: data[2]} )
+			rcv_next_waiting += 1
+			rcv_in_buffer += 1
+			print "Thread: Received packet" ,data[1] , rcv_in_buffer, "/", rcv_buffer_size
 		
 		
 			#if app waits give it priority
-			if (app_wait and next_app_read < next_waiting):
-				app_wait_mtx.release()
+			if (rcv_app_wait and rcv_next_app_read < rcv_next_waiting):
+				rcv_app_wait_mtx.release()
 				unlock = False
 				
 		if (unlock):
-			thread_app_mtx.release()
+			rcv_thread_app_mtx.release()
 
 
 
 
 
-#Globals variables!
-fd_list = []
-buffer_size = 0
+
+
+
 
 
 #create a packet ACK or Data
@@ -99,15 +129,24 @@ def deconstruct_packet(packet):
 
     return struct.unpack('!hq10s',packet)
 
+
+
+
+
+
+
+
+
+
 #Open reading side of pipe. Return a positive integer as file discriptor 
 def netfifo_rcv_open(port,bufsize):
 
     #initial buffer
-    global buffer_size
-    buffer_size = bufsize
+    global rcv_buffer_size
+    rcv_buffer_size = bufsize
 
     #create Server object (reading side)
-    socket_object = SocketServer(socket.AF_INET,socket.SOCK_DGRAM,Hostname(),port,1)
+    socket_object = SocketServer(socket.AF_INET,socket.SOCK_DGRAM,Hostname(),port,0)
 
     #start the thread
     thread.start_new_thread (rcv_thread, (socket_object, ))
@@ -121,26 +160,29 @@ def netfifo_rcv_open(port,bufsize):
 #reading from pipe. Return data.
 def netfifo_read(fd,size):
     
-    global next_app_read
-    global app_wait
-    global in_buffer
+    global rcv_next_app_read
+    global rcv_app_wait
+    global rcv_in_buffer
     
     
     #app wants to read packet next_app_read
-    thread_app_mtx.acquire()
-    if (next_app_read not in buf):
+    rcv_thread_app_mtx.acquire()
+    if (rcv_next_app_read not in rcv_buf):
 		#if packet not in buf wait
-		print "App waits for packet " ,next_app_read
-		app_wait = 1
-		thread_app_mtx.release()
-		app_wait_mtx.acquire()
-		app_wait = 0
+		print "App waits for packet " ,rcv_next_app_read
+		rcv_app_wait = 1
+		rcv_thread_app_mtx.release()
+		rcv_app_wait_mtx.acquire()
+		rcv_app_wait = 0
 
-    d = buf[next_app_read]
-    next_app_read += 1
-    in_buffer -= 1
+	
+    d = rcv_buf[rcv_next_app_read]
+    rcv_next_app_read += 1
+    rcv_in_buffer -= 1
+    del rcv_buf[rcv_next_app_read-1]
+    print "App got packet", rcv_next_app_read-1
     
-    thread_app_mtx.release()
+    rcv_thread_app_mtx.release()
 
     return d
 
@@ -152,12 +194,30 @@ def netfifo_rcv_close(fd):
 
     sock.Close()
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 #Open writing side of pipe. Return a positive integer as file discriptor
 def netfifo_snd_open(host,port,bufsize):
 
     #initial buffer
     global buffer_size
     buffer_size = bufsize
+
+
 
     #create Server object (writing side)
     socket_object = SocketClient(socket.AF_INET,socket.SOCK_DGRAM,1)
