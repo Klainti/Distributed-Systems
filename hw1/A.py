5b9281487d137b6353b5bafd811796bdc6da7a93
PanagiotisChytas <chytas96@gmail.com>
2017-03-03 14:36:15 -0800

    evala apla ena timer

--- hw1/A.py
+++ hw1/A.py
@@ -2,6 +2,8 @@ from netfifo import *
 import sys
 from struct import *
 
+import time
+
 #the socket dir contains MySocket_library.py
 sys.path.append('../sockets/')
 
@@ -11,7 +13,10 @@ import socket
 host = 'ThinkPad-Edge-E540'
 port = 10000
 
-fd = netfifo_snd_open(Hostname(),10000,10)
-netfifo_write(fd,"Hello World",10)
+fd = netfifo_snd_open(Hostname(),port,10)
+netfifo_write(fd,"Hello World",11)
+netfifo_write(fd,"Hello World",11)
+
+time.sleep (10)
 netfifo_snd_close(fd)
 
