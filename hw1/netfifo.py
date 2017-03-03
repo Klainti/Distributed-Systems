8c15cd93f0a7a3c0662efdff10e734782e233f41
PanagiotisChytas <chytas96@gmail.com>
2017-03-03 07:21:24 -0800

    ksexwrista thread kai app gia rcv

--- hw1/netfifo.py
+++ hw1/netfifo.py
@@ -10,6 +10,73 @@ from MySocket_library import *
 import socket
 import struct
 
+import thread
+import threading
+
+#mutex for app. If the packet it requests isnt in the buffer app_wait = 1 and app_wait_mtx.acquire()
+app_wait_mtx = threading.Lock()
+app_wait_mtx.acquire()
+app_wait = 0
+
+#mutex for sychronization between thread and app
+thread_app_mtx = threading.Lock()
+
+#next packet app wants to read from buffer (starts from 1)
+next_app_read = 1
+#next packet thread wants to read from socket (starts from 1)
+next_waiting = 1
+#number of packets in buffer
+in_buffer = 0
+#buffer (dictionary [key: payload])
+buf = {}
+
+
+#thread code
+def rcv_thread (sock):
+	
+	global next_waiting
+	global next_app_read
+	global in_buffer
+	global buffer_size
+	
+	while (True):
+		
+		unlock = True
+		
+		
+		print "Thread waits"
+		
+		
+		#wait for next packet
+		data = deconstruct_packet(sock.ReceiveFrom(20)[0])
+		
+		
+		print "Thread:", data
+		
+		thread_app_mtx.acquire()
+		
+		
+		#if it isnt the next packet or buffer is full throw it 
+		#else update buffer
+		if (data[1] == next_waiting and in_buffer < buffer_size):
+			buf.update( {next_waiting: data[2]} )
+			next_waiting += 1
+			in_buffer += 1
+			print "Thread: Received packet" ,data[1] , in_buffer, "/", buffer_size
+		
+		
+			#if app waits give it priority
+			if (app_wait and next_app_read < next_waiting):
+				app_wait_mtx.release()
+				unlock = False
+				
+		if (unlock):
+			thread_app_mtx.release()
+
+
+
+
+
 #Globals variables!
 fd_list = []
 buffer_size = 0
@@ -42,24 +109,40 @@ def netfifo_rcv_open(port,bufsize):
     #create Server object (reading side)
     socket_object = SocketServer(socket.AF_INET,socket.SOCK_DGRAM,Hostname(),port,1)
 
+    #start the thread
+    thread.start_new_thread (rcv_thread, (socket_object, ))
+
     fd_list.append(socket_object)
+
+
 
     return fd_list.index(socket_object)
     
 #reading from pipe. Return data.
 def netfifo_read(fd,size):
     
-    sock = fd_list[fd]
+    global next_app_read
+    global app_wait
+    global in_buffer
+    
+    
+    #app wants to read packet next_app_read
+    thread_app_mtx.acquire()
+    if (next_app_read not in buf):
+		#if packet not in buf wait
+		print "App waits for packet " ,next_app_read
+		app_wait = 1
+		thread_app_mtx.release()
+		app_wait_mtx.acquire()
+		app_wait = 0
 
-    #read data and unpack
-    data = deconstruct_packet(sock.ReceiveFrom(size)[0])
+    d = buf[next_app_read]
+    next_app_read += 1
+    in_buffer -= 1
+    
+    thread_app_mtx.release()
 
-    #data[0] = ack or data,data[1] = number of packet,data[2] = payload
-    #check the packet, payload = 1
-    if (data[0]==1):
-        return data[2]
-    else:
-        return (data[1])
+    return d
 
 
 #close reading side
