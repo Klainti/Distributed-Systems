4c58d33a78f41959dbebc6176e2922a02fb2212e
PanagiotisChytas <chytas96@gmail.com>
2017-03-07 10:33:40 -0800

    Whatever

--- hw1/B.py
+++ hw1/B.py
@@ -1,14 +1,16 @@
 import sys
 #the socket dir contains MySocket_library.py
 sys.path.append('../sockets/')
-
 from MySocket_library import *
 import socket
 from netfifo import *
+import time
+
 ##testing !
 port = 10000
-fd = netfifo_rcv_open(port,3)
-for _ in range(0,5):
-    print netfifo_read(fd,1024)
+fd = netfifo_rcv_open(port,2)
+
+s = netfifo_read(fd,30)
+print "Returned value:", s
 
 netfifo_rcv_close(fd)
