8bc7767324984b028cabffca6192b33a5235409f
PanagiotisChytas <chytas96@gmail.com>
2017-03-07 10:33:33 -0800

    Whatever

--- hw1/A.py
+++ hw1/A.py
@@ -1,12 +1,9 @@
 from netfifo import *
 import sys
 from struct import *
-
 import time
-
 #the socket dir contains MySocket_library.py
 sys.path.append('../sockets/')
-
 from MySocket_library import *
 import socket
 
@@ -14,9 +11,10 @@ host = 'ThinkPad-Edge-E540'
 port = 10000
 
 fd = netfifo_snd_open(Hostname(),port,2)
-for _ in range(0,5):
-    netfifo_write(fd,"Hello World",11)
 
-time.sleep(10)
+netfifo_write(fd,"Hello World from here",21)
+
+time.sleep(2)
+
 netfifo_snd_close(fd)
 
