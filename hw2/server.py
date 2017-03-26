222bf4d2bea67412e18efe14c677ddec54458629
Klainti <kbontourri@gmail.com>
2017-03-26 13:07:53 +0300

    Function to establish connection between client-server over TCP/IP

--- hw2/server.py
+++ hw2/server.py
@@ -2,44 +2,45 @@ import socket
 from packet_struct import *
 from multicast_module import *
 
-#Receive from multicast and tries to connect with a client
-def search_for_clients():
-
-    udp_socket = socket_for_multicast()
+def establish_connection(client_addr):
 
     #Create the TCP socket
     tcp_socket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
     tcp_socket.settimeout(TIMEOUT)
 
+    try:
+        print 'Try connecting to IP: %s, port: %d' %(client_addr[0],client_addr[1])
+	tcp_socket.connect(client_addr)
+
+
+	#Check if connection establish!
+	try:
+	    msg = tcp_socket.recv(5)
+            print 'Server: %s connected to : %s' % (tcp_socket.getsockname(),tcp_socket.getpeername())
+	    print 'Connection complete'
+            return tcp_socket
+
+	except socket.error:
+	    print "Connection failed. Try again!"
+	    tcp_socket.close()
+            return None
+
+    except socket.timeout:
+	print 'Timeout!! Try again to connect!'
+        return establish_connection(client_addr)
+
+
+#Receive from multicast and tries to connect with a client
+def search_for_clients():
+
+    udp_socket = socket_for_multicast()
+
     # Try to connect with a client
-    connection_complete = False
-    while (not connection_complete):
+    while (1):
 
         # wait for a client
         client_addr = receive_from_multicast(udp_socket)
 
-        try:
-            print 'Try connecting to IP: %s, port: %d' %(client_addr[0],client_addr[1])
-	    tcp_socket.connect(client_addr)
-
-
-	    #Check if connection establish!
-	    try:
-	        msg = tcp_socket.recv(5)
-	    except socket.error:
-	        print "Connection failed. Try again!"
-	        tcp_socket.close()
-
-    	        #delete and create a new tcp socket!
-    	        tcp_socket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-    	        tcp_socket.settimeout(TIMEOUT)
-    	        continue
-
-	    print 'Server: %s connected to : %s' % (tcp_socket.getsockname(),tcp_socket.getpeername())
-	    print 'Connection complete'
-	    connection_complete = True
-
-	except socket.timeout:
-	    print 'Trying again to connect!'
+        tcp_socket = establish_connection(client_addr)
 
 search_for_clients()
