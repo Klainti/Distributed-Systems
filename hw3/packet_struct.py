3424a71addd7e7401e6ffe2feb800e135bf1d179
Klainti <kbontourri@gmail.com>
2017-04-28 17:37:49 +0300

    Add docstring to functions

--- hw3/packet_struct.py
+++ hw3/packet_struct.py
@@ -6,16 +6,16 @@ JOIN_ENCODING = '!16si50s'
 MEMBER_CONN_DIS_ENCODING = '!50si'
 
 
-# Encode the packet for join to service!
+# Encode the packet to announce new member
 def construct_member_packet(name, Type):
-    return struct.pack(JOIN_ENCODING, name, Type)
+    return struct.pack(MEMBER_CONN_DIS_ENCODING, name, Type)
 
 
-# Encode the packet for the group details!
+# Encode the packet for a new member details!
 def construct_join_packet(ip, port, name):
     return struct.pack(JOIN_ENCODING, ip, port, name)
 
 
-# Deconstrung a packet
+# Deconstruct a packet
 def deconstruct_packet(decode, packet):
     return struct.unpack(decode, packet)
