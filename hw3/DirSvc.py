5f7fe237db84c62c5cf91abcead6d1f77ae1ab41
Klainti <kbontourri@gmail.com>
2017-04-28 17:38:33 +0300

    Fix unhashable list, now is tuple.

--- hw3/DirSvc.py
+++ hw3/DirSvc.py
@@ -8,7 +8,7 @@ about joining/leaving of a member!
 import socket
 from packet_struct import *
 
-MY_IP = ""
+MY_IP = "127.0.0.1"
 
 msggroup_sockets = {}
 
@@ -18,6 +18,7 @@ tcp_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 tcp_socket.bind((MY_IP, tcp_port))
 tcp_port = tcp_socket.getsockname()[1]
 
+print 'Service location: ({},{})'.format(MY_IP, tcp_port)
 
 while (True):
 
@@ -26,22 +27,27 @@ while (True):
     conn, addr = tcp_socket.accept()
 
     # Wait for group chat and member info
-    packet = conn.recv()
-    grpip, grpport, name = deconstruct_packet()
+    member_info_packet = conn.recv(1024)
+    grpip, grpport, name = deconstruct_packet(JOIN_ENCODING, member_info_packet)
 
-    packet = construct_member_packet(name, 1)
+    grpip = grpip.strip('\0')
+    name = name.strip('\0')
 
-    if ([grpip, grpport] not in msggroup_sockets):
+    new_member_packet = construct_member_packet(name, 1)
+
+    if ((grpip, grpport) not in msggroup_sockets):
         # First member init the group chat
-        msggroup_sockets[[grpip, grpport]] = [[conn, name]]
+        msggroup_sockets[(grpip, grpport)] = [(conn, name)]
     else:
 
         # Send to everyone that the member with name is connected
-        for member in msggroup_sockets[[grpip, grpport]]:
-            member[0].send(packet)
+        for member in msggroup_sockets[(grpip, grpport)]:
+            member[0].send(new_member_packet)
 
         # Add member to dictionary
-        msggroup_sockets[[grpip, grpport]].append([conn, name])
+        msggroup_sockets[(grpip, grpport)].append((conn, name))
+
+    print msggroup_sockets
 
     # Send that it is connected to the group chat
-    conn.send(packet)
+    conn.send(new_member_packet)
