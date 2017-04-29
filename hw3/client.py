18cfaf68f611155572707b520f9542b194ff4d88
Klainti <kbontourri@gmail.com>
2017-04-29 16:50:13 +0300

    Call join and leave

--- hw3/client.py
+++ hw3/client.py
@@ -1,45 +1,30 @@
 import socket
 from packet_struct import *
-
-tcp_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-tcp_port = tcp_socket.getsockname()[1]
+from msglib import *
 
 service_ip = raw_input("Give service IP: ")
 service_port = int(raw_input("Give service port: "))
-service_addr = (service_ip, service_port)
 
-tcp_socket.connect(service_addr)
+# set Directory service address!
+grp_setDir(service_ip, service_port)
 
-grp_ip = raw_input("Give group chat IP: ")
-grp_port = int(raw_input("Give group chat port: "))
+grp_ip = raw_input("Give grp chat IP: ")
+grp_port = int(raw_input("Give grp port: "))
 nickname = raw_input("Give nickname: ")
-request_for_grp = construct_join_packet(grp_ip, grp_port, nickname)
 
-tcp_socket.send(request_for_grp)
+# Join to a group
+gsocket = grp_join(grp_ip, grp_port, nickname)
 
-while (1):
-    reply = tcp_socket.recv(1024)
+if (gsocket == -1):
+    print 'Failed to connect'
+else:
+    while (1):
 
-    name, state = deconstruct_packet(MEMBER_CONN_DIS_ENCODING, reply)
-    name.strip('\0')
-
-    if (state == 1):
-        print name, "is connected"
-    elif (state == -1):
-        print name, "is disconnected"
-
-    exit = raw_input("Exit?[0/1] ")
-
-    if (exit == "1"):
-        tcp_socket.send ("Bye")
-        break
-
-reply = tcp_socket.recv(1024)
-
-name, state = deconstruct_packet(MEMBER_CONN_DIS_ENCODING, reply)
-name.strip('\0')
-
-if (state == 1):
-    print name, "is connected"
-elif (state == -1):
-    print name, "is disconnected"
+        send_receive = int(raw_input("Send or Receive (0/1): "))
+        if (send_receive == 0):
+            gsocket.sendto('Hello', (grp_ip, grp_port))
+        elif (send_receive == 1):
+            print gsocket.recvfrom(1024)
+        else:
+            grp_leave(gsocket)
+            break
