bc97ec7dc4d6093cec234e8fb33335d20ded64a2
Klainti <kbontourri@gmail.com>
2017-05-03 16:58:44 +0300

    Add send and receive workers

--- hw3/client.py
+++ hw3/client.py
@@ -5,28 +5,31 @@ from packet_struct import *
 from msglib import *
 
 root = None
-sending_list = []
-sending_list_lock = threading.Lock()
+sending_dict = {}
+sending_dict_lock = threading.Lock()
 
 # Join to a group
 def add_new_grp():
 
     grp_ip = "224.29.3.1"  # raw_input("Give grp chat IP: ")
     grp_port = int(raw_input("Give grp port: "))
-    nickname = "takis"  # raw_input("Give nickname: ")
+    nickname = raw_input("Give nickname: ")
 
     return grp_join(grp_ip, grp_port, nickname)
 
 
-def Enter_pressed(event, input_user):
+def Enter_pressed(event, input_widget):
 
-    input_get = input_user.get()
-    input_user.delete(0, "end")
+    input_get = input_widget.get()
+    input_widget.delete(0, "end")
 
     # import on sending_msg
-    sending_list_lock.acquire()
-    sending_list.append(input_user)
-    sending_list_lock.release()
+    sending_dict_lock.acquire()
+    if (input_widget in sending_dict.keys()):
+        sending_dict[input_widget].append(input_get)
+    else:
+        sending_dict[input_widget] = [input_get]
+    sending_dict_lock.release()
 
 
 def create_window_chat():
@@ -48,9 +51,34 @@ def create_window_chat():
 
     return (input_widget, output_widget)
 
+def send_worker(gsocket, input_widget):
+
+    global sending_dict
+
+    # Send all messages!
+    while(1):
+        sending_dict_lock.acquire()
+        if (input_widget in sending_dict.keys()):
+            if (sending_dict[input_widget] is not []):
+                for msg in sending_dict[input_widget]:
+                    grp_send(gsocket, msg)
+
+                sending_dict[input_widget] = []
+        sending_dict_lock.release()
+
+def receive_worker(gsocket, output_widget):
+
+    while(1):
+
+        msg = grp_recv(gsocket)
+        output_widget.insert(tk.END, msg)
+
 def chat_worker(gsocket):
 
     input_widget, output_widget = create_window_chat()
+
+    t_worker = threading.Thread(target=send_worker, args=(gsocket, input_widget)).start()
+    t_receiver = threading.Thread(target=receive_worker, args=(gsocket, output_widget)).start()
 
 def main_thread():
 
