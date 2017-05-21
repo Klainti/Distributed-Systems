1e4a8eceee2a2cadb1ca2422a1471f6d494589a6
PanagiotisChytas <chytas96@gmail.com>
2017-05-21 22:19:15 +0300

    Seek done

--- hw4/NFS_API.py
+++ hw4/NFS_API.py
@@ -9,8 +9,13 @@ import packet_struct
 SERVER_ADDR = ()
 udp_socket = None
 
-#counter for requests!
+# counter for requests!
+seq_num_lock = threading.Lock()
 seq_num = 0
+
+buffers_lock = threading.Lock()
+fd_pos = {}
+
 
 """Initialize connection with server"""
 def init_connection():
@@ -21,6 +26,7 @@ def init_connection():
     udp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
     # start nfs threads!
+
 
 """Set SERVER INFO"""
 def mynfs_setSrv(ipaddr, port):
@@ -33,16 +39,34 @@ def mynfs_setSrv(ipaddr, port):
     init_connection()
     return 1
 
+
 """Send Open request"""
 def mynfs_open(fname, create, cacheFreshnessT):
 
     global udp_socket, seq_num
 
+    seq_num_lock.acquire()
     seq_num += 1
+    cur_seq_num = seq_num
+    seq_num_lock.release()
 
     # create and send the open request!
-    packet_req = packet_struct.construct_open_packet(seq_num, create, fname)
+    packet_req = packet_struct.construct_open_packet(cur_seq_num, create, fname)
     udp_socket.sendto(packet_req, SERVER_ADDR)
 
     # wait for reply (JUST THE FD!)
-    return struct.unpack('!i', udp_socket.recv(4))[0]
+    fd = struct.unpack('!i', udp_socket.recv(4))[0]
+
+    buffers_lock.acquire()
+    fd_pos[fd] = 0
+    buffers_lock.release()
+
+    return fd
+
+
+"""Change pointer's position in fd"""
+def mynfs_seek(fd, pos):
+
+    buffers_lock.acquire()
+    fd_pos[fd] = pos
+    buffers_lock.release()
